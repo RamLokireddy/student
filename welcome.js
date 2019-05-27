489590e8fcbea9cadef3046c5664f3d31f0fe718
RamLokireddy <50277677+RamLokireddy@users.noreply.github.com>
2019-05-27 09:39:43 +0530

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Welcome extends Component{
+    constructor(){
+        super();
+
+        this.state={
+            clicks:0
+        }
+    }
+
+    updateClicks=()=>{
+        this.setState({
+            clicks:this.state.clicks+1
+        })
+    }
+
+    render(){
+        return <h1 onClick={this.updateClicks}>Welcome to {this.props.title} {this.props.year} - {this.state.clicks}</h1>
+    }
+}
+export default Welcome
